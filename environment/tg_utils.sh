de5ccc028a775609e191624e8c6c67e231187c09
romiyusnandar <yusromi04@gmail.com>
2024-06-27 06:54:14 +0700

    Re add proton clang script

--- environment/tg_utils.sh
+++ environment/tg_utils.sh
@@ -4,17 +4,11 @@
 
 case $1 in
   msg)
-    # curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
-    #   -d chat_id="${CHAT_ID}" \
-    #   -d parse_mode=Markdown \
-    #   -d "disable_web_page_preview=true" \
-    #   -d "parse_mode=html" \
-    #   -d text="$(echo "$2" | sed 's/%nl/\n/g')"
-
-    curl -sX POST https://api.telegram.org/bot"${BOT_TOKEN}"/sendMessage \
+    curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
       -d chat_id="${CHAT_ID}" \
       -d parse_mode=Markdown \
-      -d disable_web_page_preview=true \
+      -d "disable_web_page_preview=true" \
+      -d "parse_mode=html" \
       -d text="$(echo "$2" | sed 's/%nl/\n/g')"
   ;;
   up | upload)
