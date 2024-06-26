e1ea3847a901fb0a0fb50bf923fdae4fa46435ea
romiyusnandar <yusromi04@gmail.com>
2024-06-27 06:21:30 +0700

    Fix chat_id and bot_token

--- environment/tg_utils.sh
+++ environment/tg_utils.sh
@@ -10,8 +10,8 @@ case $1 in
     #   -d "parse_mode=html" \
     #   -d text="$(echo "$2" | sed 's/%nl/\n/g')"
 
-      curl -sX POST https://api.telegram.org/bot"${token}"/sendMessage \
-        -d chat_id="${chat_id}" \
+      curl -sX POST https://api.telegram.org/bot"${BOT_TOKEN}"/sendMessage \
+        -d chat_id="${CHAT_ID}" \
         -d parse_mode=Markdown -\
         -d disable_web_page_preview=true \
         -d text="$1" &>/dev/null
