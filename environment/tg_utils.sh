afb816fb0fcca0b36d93311fb21c0f809856f238
romiyusnandar <yusromi04@gmail.com>
2024-06-27 07:20:24 +0700

    Fix telegram

--- environment/tg_utils.sh
+++ environment/tg_utils.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#
+# ...exists to not have ugly looking code blocks in ci lmao
 
 case $1 in
   msg)
@@ -8,7 +8,7 @@ case $1 in
       -d chat_id="${CHAT_ID}" \
       -d "disable_web_page_preview=true" \
       -d "parse_mode=html" \
-      -d text="$(echo "$2" | sed 's/%nl/\n/g
+      -d text="$(echo "$2" | sed 's/%nl/\n/g')"
   ;;
   up | upload)
     curl -F chat_id="${CHAT_ID}" \
