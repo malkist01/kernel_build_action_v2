4576412707ba3f7b0089c0db0d63c71e3fc2cdd3
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-10 20:09:55 +0700

    Update apply-patches.sh

--- environment/ksu/apply-patches.sh
+++ environment/ksu/apply-patches.sh
@@ -1,1 +1,6 @@
-tes
+maindir="$(pwd)"
+rm -rf KernelSU
+
+# integrate kernelsu-SukiSu
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+
